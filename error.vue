db53870c30f58660910fcff7367ae0763a2df277
Dan Farrow <dan@zellement.com>
2025-06-08 10:03:52 +0100

    add error.vue

---
+++ error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container container-px flex h-screen flex-col text-center">
+        <div class="m-auto">
+            <h1 class="h1">Sorry!</h1>
+            <h2 class="h4">We couldn't find the page you were looking for.</h2>
+            <p class="mt-5">
+                <a class="text-blue-500" href="/">Return to the homepage</a>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
